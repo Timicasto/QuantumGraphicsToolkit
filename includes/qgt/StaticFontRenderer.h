51fa0729681239b3576815fa34dede6b946d9b00
Timicasto Index <timicasto@gmail.com>
2023-03-20 02:50:17 +0800

    fix: memory leakage

--- includes/qgt/StaticFontRenderer.h
+++ includes/qgt/StaticFontRenderer.h
@@ -137,6 +137,16 @@ public:
 		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
+		
+		for (int i = 0 ; i < text.size() ; ++i) {
+			delete[] data[i];
+		}
+		delete[] data;
+		
+		for (int i = 0 ; i < totalW ; ++i) {
+			delete[] texture[i];
+		}
+		delete[] texture;
 	}
 
 	void render() {
